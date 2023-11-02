8724220a4b7b9d0b071516f09b63dd15d0d52a8c
Caroline <wanguic49@gmail.com>
2023-11-02 12:39:25 +0300

    few changes made

---
+++ src/app/services/service-cart.service.ts
@@ -0,0 +1,25 @@
+import { Product } from "../products";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class CartService{
+    items: Product[] = [];
+
+    addToCart(product:Product){
+        this.items.push(product);
+        // console.log(this.items.length);
+    }
+
+    getItems(){
+        return this.items;
+    }
+
+    clearCart(){
+        this.items = [];
+        return this.items;
+    }
+
+}
